5d76cf6c81539eb0f24c34ec419e8872773b1c99
pryority <matthewapryor@gmail.com>
2024-06-12 13:50:30 -0400

    🚚 Rename App

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
-import { cn } from "@/lib/utils";
 import { Providers } from "./providers";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import Header from "@/components/Header";
+import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 export const metadata: Metadata = {
-  title: "Trumen World",
+  title: "React Native Smart Wallet Integration",
   description: "For Coinbase Onchain Summer Hackathon",
 };
 
