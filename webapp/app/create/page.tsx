52f38f2a64774e405583ff534b3b1dd44ada79a9
pryority <matthewapryor@gmail.com>
2024-06-12 22:55:42 -0400

    🎨 Update

--- webapp/app/create/page.tsx
+++ webapp/app/create/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 import Page from "@/components/Page";
+import { Button } from "@/components/ui/button";
 import useUser, { type UserState } from "@/lib/hooks/use-user";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
-import { useEffect } from "react";
 import { base } from "viem/chains";
-import { createConfig, http, useAccountEffect } from "wagmi";
+import { createConfig, http } from "wagmi";
 import { useAccount, useConnect } from "wagmi";
 import { coinbaseWallet } from "wagmi/connectors";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import AppReturn from "@/components/AppReturn";
+import { useEffect, useState } from "react";
 
 export default function Create() {
+  const [appUrl, setAppUrl] = useState<string>();
   const [user, setUser] = useUser();
   const { connect, status, error } = useConnect();
   const config = createConfig({
@@ -23,54 +32,59 @@ export default function Create() {
     transports: { [base.id]: http() },
   });
   const account = useAccount({ config });
+  const chainId = 8453;
+  const connector = coinbaseWallet({
+    appName: "Coinbase Smart Wallet w/ React Native",
+    preference: "smartWalletOnly",
+  });
 
-  useAccountEffect({
-    onConnect(data) {
-      handleJumpToRNApp()
-    },
-    onDisconnect() {
-      console.log('Disconnected!')
-    },
-  })
-
-  function handleJumpToRNApp() {
-    if (!user.account?.address) return;
-
-    const appUrl = `RNCBSmartWallet://address?address=${encodeURIComponent(user.account.address)}`;
-    console.log("appUrl", appUrl);
-    window.location.href = appUrl;
-
-    return;
+  async function handleConnect() {
+    try {
+      connect({
+        chainId,
+        connector,
+      });
+      setUser((prevState: UserState) => ({ ...prevState, account }));
+    } catch (err) {
+      console.error("Connection failed", err);
+    }
   }
 
-  function handleCreateButtonPress() {
-    connect({
-      chainId: 8453,
-      connector: coinbaseWallet({
-        appName: "Coinbase Smart Wallet w/ React Native",
-        preference: "smartWalletOnly",
-      }),
-    });
-    setUser((prevState: UserState) => ({ ...prevState, account }));
-  }
+  useEffect(() => {
+    console.log(user.account?.address);
+    if (user.account?.address) {
+      const APP_URL = `RNCBSmartWallet://?address=${encodeURIComponent(user.account.address)}`;
+      setAppUrl(APP_URL);
+    }
+  }, [user.account?.address]);
 
   return (
     <Page>
-      <p className="p-4 flex flex-col w-72 text-center">
-        <span className="text-blue-500 font-bold">CREATE</span>
-        {/* make this a button "" */}
-        <span
-          onClick={handleCreateButtonPress}
-        >
-        click here to connect
-        </span>
-        
-        {user.account?.address && <span
-          onClick={handleJumpToRNApp}
-        >
-        click here to go back to the app
-        </span>}
+      <p className="p-4 flex flex-col w-96 text-center">
+        <span className="text-blue-500 font-bold">{`CREATE & CONNECT`}</span>
       </p>
+
+      {!user.account?.address ? (
+        <Card>
+          <CardHeader>
+            <CardTitle>Connect Wallet</CardTitle>
+            <CardDescription>Approve the Coinbase Connection</CardDescription>
+          </CardHeader>
+          <CardContent className="items-center flex flex-col">
+            <Button type="button" onClick={handleConnect}>
+              Connect Wallet
+            </Button>
+          </CardContent>
+        </Card>
+      ) : (
+        <AppReturn
+          appUrl={appUrl}
+          title="Successfully Connected!"
+          description=""
+          message="Return and Sign-In With Ethereum"
+        />
+      )}
+
       <p className="p-4 flex flex-col w-96 text-center">
         Connection Status:
         <span
