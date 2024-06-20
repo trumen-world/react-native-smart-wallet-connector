80514d2772baa7de75718d1f506f695100c9426c
pryority <matthewapryor@gmail.com>
2024-06-19 21:15:46 -0400

    ➕ Add skeleton for loading

--- next/app/sign/page.tsx
+++ next/app/sign/page.tsx
@@ -1,27 +1,14 @@
 import Page from "@/components/Page";
 import Signer from "@/components/Signer";
-import AppReturn from "@/components/AppReturn";
-import UserAddress from "@/components/UserAddress";
+import SignerSkeleton from "@/components/SignerSkeleton";
+import { Suspense } from "react";
 
-export default async function Sign({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  // if (!searchParams.address) {
-  //   return <AppReturn message="No Account Param Provided From React Native" />;
-  // }
-  // if (!searchParams.message) {
-  //   return <AppReturn message="No Message Param Provided From React Native" />;
-  // }
-  // const ADDRESS_PARAM = searchParams.address?.toString();
-  // console.log("Address Param: ", ADDRESS_PARAM);
-  // const MESSAGE_PARAM = searchParams.message?.toString();
-  // console.log("Message Param: ", MESSAGE_PARAM);
-
+export default async function Sign() {
   return (
     <Page>
-      <Signer />
+      <Suspense fallback={<SignerSkeleton />}>
+        <Signer />
+      </Suspense>
     </Page>
   );
 }
